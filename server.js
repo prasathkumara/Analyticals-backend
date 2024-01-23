bdc6d8e465e81a9f0108626490f15adc362d4995
prasathkumara <prasathkumar.a@cynnent.com>
2024-01-23 20:19:58 +0530

    mongo url

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-// import { MongoClient } from "mongodb";
-
-// const mongoURI= "mongodb://127.0.0.1:27017"
-// async function createConnection(){
-//     const client = new MongoClient(mongoURI);
-//     await client.connect()
-//     console.log("mongodb is connected suucessfully")
-//     return client
-// }
-//  export {createConnection}
-
-// server.js
 
 const mongoose = require('mongoose');
-const mongoURL = "mongodb+srv://prasath12:prasath123@cluster0.btxw1s3.mongodb.net/web";
+const mongoURL = "mongodb+srv://cynnent:cynnent123@cluster0.0ybxpvk.mongodb.net/web";
 
 const dbConnection = async () => {
   try {
