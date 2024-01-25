aea2b60ca89eca330a6c136101aa1047ce06f0b5
prasathkumara <prasathkumar.a@cynnent.com>
2024-01-25 09:11:35 +0530

    update

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,21 +1,17 @@
 const { dbConnection } = require("../server");
-
 const storeData = async (req, res) => {
   try {
     const db = await dbConnection();
     const userData = req.body;
     const userIp = userData.userInfo[0].ip;
+    console.log(userIp)
 
     // Get the current date and time
     const currentDate = new Date();
-    //currentDate.setDate(currentDate.getDate() + 2);
-    const formattedDate = currentDate.toISOString().slice(0, 10); 
-
-     // Get the current day
-     const currentDayNumber = currentDate.getDay();
-     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-     const currentDay = daysOfWeek[currentDayNumber];
-
+    currentDate.setDate(currentDate.getDate() + 4);
+   
+    const formattedDate = currentDate.toISOString().slice(0, 10);
+ console.log(formattedDate)
     // Fetch existing user data based on IP address
     const existingUser = await db.collection('userEvents').findOne({
       'userInfo.ip': userIp,
@@ -27,22 +23,11 @@ const storeData = async (req, res) => {
 
       if (existingEventData) {
         // Update the existing data for the current date
-        const updatedUserEvents = existingUser.userEvents.map(event => {
-          if (event.date === formattedDate) {
-            return {
-              date: formattedDate,
-              day:currentDay,
-              ...Object.keys(userData.userEvents[0]).reduce((acc, screen) => {
-                acc[screen] = {
-                  ...(event[screen] || {}),
-                  ...(userData.userEvents[0][screen] || {}),
-                };
-                return acc;
-              }, {}),
-            };
-          }
-          return event;
-        });
+        const updatedScreen = {
+          ...(existingEventData || {}),
+          ...(userData.userEvents[0] || {})
+        };
+
 
         await db.collection('userEvents').updateOne(
           {
@@ -51,14 +36,14 @@ const storeData = async (req, res) => {
           },
           {
             $set: {
-              'userEvents': updatedUserEvents,
-              'userInfo': [userData.userInfo[0]],
+              'userEvents.$': updatedScreen,
+              'userInfo': [userData.userInfo[0]]
             }
           }
         );
 
         console.log('Data updated in MongoDB');
-        res.status(200).json({ message: 'Data updated successfully', data: userData });
+        res.status(200).json({ message: 'Data updated successfully',data:userData });
       } else {
         // Insert new data for the current date
         await db.collection('userEvents').updateOne(
@@ -68,18 +53,15 @@ const storeData = async (req, res) => {
             $addToSet: {
               'userEvents': {
                 date: formattedDate,
-                day:currentDay,
-                ...Object.keys(userData.userEvents[0]).reduce((acc, screen) => {
-                  acc[screen] = userData.userEvents[0][screen] || {};
-                  return acc;
-                }, {}),
+                ...userData.userEvents[0]|| {},
+                
               }
             }
           }
         );
 
         console.log('Data inserted into MongoDB');
-        res.status(200).json({ message: 'Data stored successfully', data: userData });
+        res.status(200).json({ message: 'Data stored successfully', data:userData});
       }
     } else {
       // Insert data into MongoDB for a new user on the specified date
@@ -87,11 +69,8 @@ const storeData = async (req, res) => {
         'userInfo': [userData.userInfo[0]],
         'userEvents': [{
           date: formattedDate,
-          day:currentDay,
-          ...Object.keys(userData.userEvents[0]).reduce((acc, screen) => {
-            acc[screen] = userData.userEvents[0][screen] || {};
-            return acc;
-          }, {}),
+          ...userData.userEvents[0] || {},
+         
         }]
       });
 
@@ -105,6 +84,7 @@ const storeData = async (req, res) => {
 };
 
 
+
 const getAllData= async(req, res) => {
   try {
     const db = await dbConnection();
@@ -112,7 +92,7 @@ const getAllData= async(req, res) => {
     // Retrieve all data from MongoDB using Mongoose
     const allData = await db.collection('userEvents').find({}).toArray();
 
-    res.status(200).json({allData});
+    res.status(200).json(allData);
   } catch (err) {
     console.error('Error retrieving data from MongoDB:', err);
     res.status(500).send('Internal Server Error');
